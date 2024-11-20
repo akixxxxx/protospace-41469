e3d1f6495148fc17b1139eaa88a7bd2d7e055bef
akixxxxx <akixxxxx01@gmail.com>
2024-11-20 20:43:47 +0900

    prototypeのバリデーションを設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,9 @@ class Prototype < ApplicationRecord
   has_many :user
   belongs_to :comments
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
 end
