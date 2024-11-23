ce350ed0534abb12b9ed435891ed2f2a0b45396b
akixxxxx <akixxxxx01@gmail.com>
2024-11-23 17:46:26 +0900

    new.html.erbから部分テンプレートである、_form.html.erbを呼び出す記述をしたの修正しました

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,10 +5,21 @@ class PrototypesController < ApplicationController
   end
 
   def new
-    @prototype = Prototype.all
+    @prototype = Prototype.new
   end
 
   def create
     @prototype = Prototype.all
+    Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: 'Prototype was successfully created.' 
+    else
+      render :new
+    end
+  end
+
+  private 
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
