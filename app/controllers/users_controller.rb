77dbc3d982e7b124628ae816f25fc64cabcdfad6
akixxxxx <akixxxxx01@gmail.com>
2024-12-01 21:47:52 +0900

    applicationコントローラの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,7 @@
 class UsersController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-
-  def index
-    @user = Users.all
-  end
-
-  def delete
-    room = Room.find(params[:id])
-    room.destroy
-    redirect_to root_path
-  end
 
   def show
     @user = User.find(params[:id])
-  end
-
-  private
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 
 end
